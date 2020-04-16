ec80a4745fe0b938b9d8ab906c755aea41adb2bc
Bentroen <bemcdc@gmail.com>
2020-04-15 21:24:14 -0300

    Improve general project structure
    The entry point main.py contains only the basic code necessary for the application to start. app.py stores the program logic (e.g. parse command line arguments, set icon/window title, set flags for high DPI support), while the actual visible contents of the window reside in its own module, mainwindow.py.
    
    See:
    https://stackoverflow.com/questions/22177976/generally-speaking-how-are-python-projects-structured
    https://stackoverflow.com/questions/21826516/pyqt-project-organization

---
+++ main.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    import sys
+    from nbs import app
+    sys.exit(app.main())
---
+++ nbs/mainwindow.py
@@ -0,0 +1,25 @@
+from PyQt5 import QtWidgets
+import nbs.ui.components
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Minecraft Note Block Studio")
+        self.setMinimumSize(854, 480)
+
+        menuBar = nbs.ui.components.drawMenuBar(self)
+        toolBar = nbs.ui.components.drawToolBar(self)
+        mainArea = nbs.ui.components.CentralArea(self)
+
+        self.setMenuBar(menuBar)
+        self.addToolBar(toolBar)
+        self.setCentralWidget(mainArea)
+
+    def drawMenuBar(self):
+        pass
+
+    def drawToolBar(self):
+        pass
+
+    def drawStatusBar(self):
+        pass
