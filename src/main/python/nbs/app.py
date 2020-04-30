93a2252feddf29065cd29eea81bb2b01a381e4ee
Bentroen <bemcdc@gmail.com>
2020-04-30 15:47:00 -0300

    Update app.py to conform to fbs project structure

--- src/main/python/nbs/app.py
+++ src/main/python/nbs/app.py
@@ -1,21 +1,21 @@
-import sys
 import os
 from PyQt5 import QtCore, QtWidgets
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from nbs.mainwindow import MainWindow
 
-def main():
-    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+class AppContext(ApplicationContext):
+    def run(self):
+        os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 
-    # Create application
-    app = QtWidgets.QApplication(sys.argv)
-    app.setApplicationName("Minecraft Note Block Studio")
+        # Set application info
+        self.app.setApplicationName("Minecraft Note Block Studio")
 
-    # Enable support for high DPI displays and use high res icons
-    app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
-    app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+        # Enable support for high DPI displays and use high res icons
+        self.app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+        self.app.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
-    # Create main window
-    window = MainWindow()
-    window.show()
+        # Create main window
+        window = MainWindow()
+        window.show()
 
-    return app.exec_()
+        return self.app.exec_()
