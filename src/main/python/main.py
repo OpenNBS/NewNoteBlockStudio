5c4a17cb0d7bed0fd231cb108a3ab15c8ff0c683
Bentroen <bemcdc@gmail.com>
2020-04-30 15:46:17 -0300

    Move files to conform to fbs project structure

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,7 @@
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
-
+from nbs.app import AppContext
 import sys
 
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+    appctxt = AppContext()
+    exit_code = appctxt.run()
     sys.exit(exit_code)
