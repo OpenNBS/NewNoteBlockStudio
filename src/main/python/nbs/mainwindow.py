c263334ee448d9c947998bfb6425413704c7b9a7
Bentroen <bemcdc@gmail.com>
2020-05-01 18:49:40 -0300

    Add functional .nbs import example
    As pointed out in #2, pynbs currently only supports classic and v3 .nbs files. The provided song was saved in version 3 and imports just as expected!

--- src/main/python/nbs/mainwindow.py
+++ src/main/python/nbs/mainwindow.py
@@ -17,14 +17,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.addToolBar(toolBar)
         self.setCentralWidget(mainArea)
 
-        current_song = nbs.core.data.Song(song_dict={"header": None, "layers": None, "instruments": None,
-                                                     "notes": [{"tick": 0, "layer": 1, "key": 1, "instrument": 0},
-                                                               {"tick": 1, "layer": 2, "key": 2, "instrument": 0},
-                                                               {"tick": 2, "layer": 3, "key": 3, "instrument": 0},
-                                                               {"tick": 3, "layer": 4, "key": 4, "instrument": 0},
-                                                               {"tick": 4, "layer": 5, "key": 5, "instrument": 0}]})  # testing
+       # current_song = nbs.core.data.Song(song_dict={"header": None, "layers": None, "instruments": None,
+       #                                              "notes": [{"tick": 0, "layer": 1, "key": 1, "instrument": 0},
+       #                                                        {"tick": 1, "layer": 2, "key": 2, "instrument": 0},
+       #                                                        {"tick": 2, "layer": 3, "key": 3, "instrument": 0},
+       #                                                        {"tick": 3, "layer": 4, "key": 4, "instrument": 0},
+       #                                                        {"tick": 4, "layer": 5, "key": 5, "instrument": 0}]})  # testing
+
+        current_song = nbs.core.data.Song(filename="test.nbs")
+
         for note in current_song.notes:
-            mainArea.workspace.noteBlockWidget.addBlock(note["tick"], note["layer"], note["key"], note["instrument"])
+            mainArea.workspace.noteBlockWidget.addBlock(note.tick, note.layer, note.key, note.instrument)
 
     def drawMenuBar(self):
         menuBar = QtWidgets.QMenuBar(parent=self)
