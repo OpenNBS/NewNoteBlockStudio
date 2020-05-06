6f593f1713d22bc866ed7af49646b44e5b111c9e
LachlanDK <lachlandufort-kennett@outlook.com>
2020-05-06 09:58:47 +0100

    Removed old .nbs load test

--- src/main/python/nbs/mainwindow.py
+++ src/main/python/nbs/mainwindow.py
@@ -3,6 +3,7 @@ import qtawesome as qta
 import nbs.ui.components
 import nbs.core.data
 
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -17,17 +18,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.addToolBar(toolBar)
         self.setCentralWidget(mainArea)
 
-       # current_song = nbs.core.data.Song(song_dict={"header": None, "layers": None, "instruments": None,
-       #                                              "notes": [{"tick": 0, "layer": 1, "key": 1, "instrument": 0},
-       #                                                        {"tick": 1, "layer": 2, "key": 2, "instrument": 0},
-       #                                                        {"tick": 2, "layer": 3, "key": 3, "instrument": 0},
-       #                                                        {"tick": 3, "layer": 4, "key": 4, "instrument": 0},
-       #                                                        {"tick": 4, "layer": 5, "key": 5, "instrument": 0}]})  # testing
+        self.currentSong = nbs.core.data.Song()  # Initialize empty song object at the start of session
 
-        current_song = nbs.core.data.Song(filename="test.nbs")
-
-        for note in current_song.notes:
-            mainArea.workspace.noteBlockWidget.addBlock(note.tick, note.layer, note.key, note.instrument)
+        self.load_song("test.nbs")  # Testing
 
     def drawMenuBar(self):
         menuBar = QtWidgets.QMenuBar(parent=self)
@@ -152,3 +145,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def drawStatusBar(self):
         pass
+
+    def load_song(self, filename):
+        """Loads a .nbs file into the current session."""
+        # TODO: import header details, layers, etc.
+        self.currentSong = nbs.core.data.Song(filename=filename)
+        for note in self.currentSong.notes:
+            self.centralWidget().workspace.noteBlockWidget.addBlock(note.tick, note.layer, note.key, note.instrument)
