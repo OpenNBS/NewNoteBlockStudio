cd9a024e1544be8f8517d2ce15bb2b6f50cdbe7e
Bentroen <bemcdc@gmail.com>
2022-05-28 02:31:03 -0300

    Fix incorrect selected icon attribute

--- src/main/python/nbs/ui/actions.py
+++ src/main/python/nbs/ui/actions.py
@@ -22,7 +22,7 @@ class Actions(QtCore.QObject):
             "save_song_as":     qta.icon('mdi.content-save-all'),
             "rewind":           qta.icon('mdi.rewind'),
             "fast_forward":     qta.icon('mdi.fast-forward'),
-            "play_pause":       qta.icon('mdi.play', checked='mdi.pause'),
+            "play_pause":       qta.icon('mdi.play', selected='mdi.pause'),
             "stop":             qta.icon('mdi.stop'),
             "record":           qta.icon('mdi.record'),
             "loop":             qta.icon('mdi.repeat'),
